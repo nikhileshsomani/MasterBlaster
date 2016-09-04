25fad7ca8e1d3df45a8ad75c1ad6ffd8c5be4eb6
Nikhilesh Somani <nikhileshsomani@github.com>
2016-09-04 16:58:24 +0530

    design changes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,6 +15,13 @@ angular.module('sachinRtApp')
 		var countries = [],
 			years = [];
 
+		$scope.totalRuns = null;
+		$scope.totalMatches = stats.length;
+		$scope.totalWickets = null;
+		$scope.totalCatches = null;
+		$scope.totalHundreds = null;
+		$scope.totalFifties = null;
+
 		$scope.sachinQuotes = ['There are two kind of batsmen in the world. One Sachin Tendulkar. Two all the others. - Andy Flower',
 			'I have seen God, he bats at no. 4 for India - Mathew Hayden',
 			'He can play that leg glance with a walking stick also. - Waqar Younis'
@@ -22,6 +29,15 @@ angular.module('sachinRtApp')
 
 		var countriesObject = [],
 			runsPerYear = [],
+			runsPerInnings = [{
+				'label': 'Runs while chasing',
+				'value': 0,
+				'displayValue': "0"
+			}, {
+				'label': 'Runs while batting first',
+				'value': 0,
+				'displayValue': "0"
+			}],
 			scoreByTwoHundreds = [],
 			scoreByHundreds = [],
 			scoreByFifties = [],
@@ -49,7 +65,8 @@ angular.module('sachinRtApp')
 			if (countryIdx < 0) {
 				countries.push(el.opposition);
 				countriesObject.push({
-					'label': el.opposition
+					'label': el.opposition,
+					// 'displayValue': '0'
 				});
 				scoreByTwoHundreds.push({
 					'value': null
@@ -65,22 +82,45 @@ angular.module('sachinRtApp')
 				});
 				countryIdx = countriesObject.length - 1;
 			}
-			if (el.batting_score) {
+			if (!isNaN(el.batting_score)) {
+				// countriesObject[countryIdx].displayValue = (parseInt(countriesObject[countryIdx].displayValue) + 1).toString() + " Matches";
 				if (el.batting_score < 50) {
 					scoreByRest[countryIdx].value += el.batting_score;
 				} else if (el.batting_score >= 50 && el.batting_score < 100) {
+					$scope.totalFifties++;
 					scoreByFifties[countryIdx].value += el.batting_score;
 				} else if (el.batting_score >= 100 && el.batting_score < 200) {
+					$scope.totalHundreds++;
 					scoreByHundreds[countryIdx].value += el.batting_score;
 				} else {
+					$scope.totalHundreds++;
 					scoreByTwoHundreds[countryIdx].value += el.batting_score;
 				}
 			}
 		};
 
+		var setRunsPerInning = function(el) {
+			if (!isNaN(el.batting_score)) {
+				if (el.batting_innings === "2nd") {
+					runsPerInnings[0].value += el.batting_score;
+					runsPerInnings[0].displayValue = (parseInt(runsPerInnings[0].displayValue) + 1).toString() + " Matches while chasing";
+				} else {
+					runsPerInnings[1].value += el.batting_score;
+					runsPerInnings[1].displayValue = (parseInt(runsPerInnings[1].displayValue) + 1).toString() + " Matches while batting first";
+				}
+			}
+		};
+
 		stats.forEach(function(el) {
+			if (!isNaN(el.batting_score)) {
+				$scope.totalRuns += el.batting_score;
+			}
+			if (!isNaN(el.wickets)) {
+				$scope.totalWickets += el.wickets;
+			}
 			setRunsPerYear(el);
 			setRunsPerCountry(el);
+			setRunsPerInning(el);
 		});
 
 		$scope.countryWiseScore = {
@@ -132,7 +172,7 @@ angular.module('sachinRtApp')
 		$scope.runsPerYear = {
 			"chart": {
 				"caption": "SACHIN TENDULKAR'S ODI CAREER",
-				"subcaption": "(Yearly)",
+				"subcaption": "(Batting)",
 				"xAxisName": "Years",
 				"yAxisName": "Runs",
 				"lineThickness": "2",
@@ -163,8 +203,40 @@ angular.module('sachinRtApp')
 				"showAlternateHGridColor": "0"
 			},
 			"data": runsPerYear,
-
 		};
 
+		$scope.scorePerInnings = {
+			"chart": {
+				"caption": "Runs scored per innings",
+				"paletteColors": "#0075c2,#1aaf5d,#f2c500,#f45b00,#8e0000",
+				"bgColor": "#ffffff",
+				"showBorder": "0",
+				"use3DLighting": "0",
+				"showShadow": "0",
+				"enableSmartLabels": "0",
+				"startingAngle": "0",
+				"showPercentValues": "1",
+				"showPercentInTooltip": "0",
+				"decimals": "1",
+				"captionFontSize": "14",
+				"subcaptionFontSize": "14",
+				"subcaptionFontBold": "0",
+				"toolTipColor": "#ffffff",
+				"toolTipBorderThickness": "0",
+				"toolTipBgColor": "#000000",
+				"toolTipBgAlpha": "80",
+				"toolTipBorderRadius": "2",
+				"toolTipPadding": "5",
+				"showHoverEffect": "1",
+				"showLegend": "1",
+				"legendBgColor": "#ffffff",
+				"legendBorderAlpha": "0",
+				"legendShadow": "0",
+				"legendItemFontSize": "10",
+				"legendItemFontColor": "#666666",
+				"useDataPlotColorForLabels": "1"
+			},
+			"data": runsPerInnings
+		};
 
 	});
